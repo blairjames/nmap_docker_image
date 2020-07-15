0f5c612005890bf4c95519484032b5a6f6fe6443
Blair <blair@blairjames.com>
2020-07-15 14:34:09 +1000

    Automatic build 2020-07-15_14-34-09

--- trigger_build.sh
+++ trigger_build.sh
@@ -8,7 +8,7 @@ timestamp() {
 
 
 
-git add --all  
+git add Dockerfile 
 git commit -m "Automatic build $(timestamp)"
-git push https://blairjames:5vBq!Z%47k@github.com/blairjames/nmap_docker_image --all
+git push https://github.com/blairjames/nmap_docker_image
 
