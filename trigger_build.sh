aca8e6c40e61a27e6c8017f66e3844d679872c6d
Blair <blair@blairjames.com>
2020-07-17 21:58:05 +1000

    Automatic build $timestp

--- trigger_build.sh
+++ trigger_build.sh
@@ -38,7 +38,7 @@ fi
 # Push to github - Triggers builds in github and Dockerhub.
 git="/usr/bin/git -C /home/docker/nmap/nmap_docker_image/"
 
-$git pull >> $log || logger "git pull failed!"
+/usr/bin/git -C /home/docker/nmap/nmap_docker_image/ pull >> $log || logger "git pull failed!"
 $git add --all >> $log || logger "git add failed!"
 $git commit -a -m 'Automatic build $timestp' >> $log || logger "git commit failed!"
 $git push >> $log || logger "git push failed!"
