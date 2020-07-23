00ba4bbfdcafb8c6556462fa81d3849f1d6f7dc7
Blair <blair@blairjames.com>
2020-07-23 20:06:54 +1000

    Automatic build $timestp

--- trigger_build.sh
+++ trigger_build.sh
@@ -1,32 +1,17 @@
 #!/bin/bash
 
+# Env Vars for SSH.
+source /root/.ssh/agent/root
+
 # Log file
 log="/home/docker/nmap/nmap_docker_image/log_nmap_docker_image.log"
 
-# Setup SSH key management
-# TODO: Add test for existing ssh-agent - nb. The file with the env vars 
-# needs to be current with the running pid. So at some point we need 
-# to run and capture the ssh-agent cmd and add the pvt key. 
-# need to test if ssh-agent runs at start up etc 
-# make this a function!
-
-is_ssh_agent_running () {
-    pid = "ps aux | grep -i ssh-agent | grep -iv defunct | grep -iv grep | cut -c 11-16" 
-    pid_env = "cat /root/.ssh/agent/root | tail -n 1 | cut -c 15-20"
-}
-
-start_ssh_agent () {
-    pkill ssh-agent
-    sleep 2
-    ssh-agent -s | grep -v echo > "/root/.ssh/agent/root"
-    source /root/.ssh/agent/root
-    ssh-add "/root/.ssh/blair_at_blairjames.com"
-}
 
 # Generate timestamp
 timestamp () {
     date +"%Y%m%d_%H%M%S"
 }
+
 
 # Log and Print
 logger () {
@@ -34,9 +19,11 @@ logger () {
     printf "$(timestamp) - $1\n" >> $log
 }
 
+
 # Assign timestamp to ensure var is a static point in time.
 timestp=$(timestamp)
 logger "Starting Build.\n"
+
 
 # Build the image using timestamp as tag.
 if /usr/bin/docker build /home/docker/nmap/nmap_docker_image -t docker.io/blairy/nmap:$timestp >> $log; then
@@ -46,6 +33,7 @@ else
     exit 1
 fi
 
+
 # Test - If test pass, commit and push to github and Dockerhub.
 if /home/docker/nmap/nmap_docker_image/test_script_nmap.py docker.io/blairy/nmap:$timestp >> $log; then
     logger "Tests completed successfully.\n\n"
@@ -53,6 +41,7 @@ else
     logger "******  WARNING!!  --  Tests FAILED!!  Aborting. ******\n\n"
     exit 1
 fi
+
 
 # Push to github - Triggers builds in github and Dockerhub.
 # TODO: Make this a function and add better exception management.. 
@@ -63,6 +52,7 @@ $git add --all >> $log || logger "git add failed!"
 $git commit -a -m 'Automatic build $timestp' >> $log || logger "git commit failed!"
 $git push >> $log || logger "git push failed!"
 
+
 # Push the new tag to Dockerhub.
 if docker push blairy/nmap:$timestp >> $log; then 
     logger "Docker push completed successfully.\n\n"
@@ -70,6 +60,7 @@ else
     logger "Docker push FAILED!!\n\n"
     exit 1 
 fi
+
 
 # All completed successfully
 logger "All completed successfully"
