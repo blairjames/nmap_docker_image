f52e0a10812fb4f1bdea8ecf0804436336c52288
Blair <blair@blairjames.com>
2020-08-06 22:07:14 +1000

    Automatic build 20200806_220601

--- trigger_build.sh
+++ trigger_build.sh
@@ -19,18 +19,15 @@ logger () {
     printf "$(timestamp) - $1\n" >> $log
 }
 
-
 # Exception Catcher
 except () {
     logger $1
     return 1
 }
 
-
 # Assign timestamp to ensure var is a static point in time.
 timestp=$(timestamp)
 logger "Starting Build.\n"
-
 
 # Build the image using timestamp as tag.
 if /usr/bin/docker build /home/docker/nmap/nmap_docker_image -t docker.io/blairy/nmap:$timestp >> $log; then
@@ -39,7 +36,6 @@ else
     logger "Build FAILED!! Aborting.\n\n"
     exit 1
 fi
-
 
 # Test - If test pass, commit and push to github and Dockerhub.
 if /home/docker/nmap/nmap_docker_image/test_script_nmap.py docker.io/blairy/nmap:$timestp >> $log; then
@@ -57,7 +53,7 @@ git () {
     git="/usr/bin/git -C /home/docker/nmap/nmap_docker_image/"
     $git -C '/home/docker/nmap/nmap_docker_image/' pull git@github.com:blairjames/nmap_docker_image.git >> $log || except "git pull failed!"
     $git add --all >> $log || except "git add failed!"
-    $git commit -a -m 'Automatic build $timestp' >> $log || except "git commit failed!"
+    $git commit -a -m 'Automatic build '$timestp >> $log || except "git commit failed!"
     $git push >> $log || except "git push failed!"
 }
 
@@ -69,7 +65,6 @@ else
     logger "git failed!!" 
 fi
 
-
 # Push the new tag to Dockerhub.
 if docker push blairy/nmap:$timestp >> $log; then 
     logger "Docker push completed successfully.\n\n"
@@ -77,7 +72,6 @@ else
     logger "Docker push FAILED!!\n\n"
     exit 1 
 fi
-
 
 # All completed successfully
 logger "All completed successfully"
