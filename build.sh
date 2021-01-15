7ed79204203f3d4ef9c01f8da5e4e8d577d3847c
Blair <blair@blairjames.com>
2021-01-16 06:16:10 +1000

    Automatic build

--- build.sh
+++ build.sh
@@ -4,11 +4,13 @@
 source /root/.ssh/agent/root || . /root/.ssh/agent/root
 
 # Log file
-log="/home/docker/nmap/nmap_docker_image/log_nmap_docker_image.log"
+declare -r log="/home/docker/nmap/nmap_docker_image/log_nmap_docker_image.log"
 
 # Generate timestamp
 timestamp () {
-    date +"%Y%m%d_%H%M%S"
+  local ts
+  ts=date +"%Y%m%d_%H%M%S"
+  return $ts
 }
 
 # Log and Print
@@ -17,61 +19,49 @@ logger () {
     printf "$(timestamp) - $1\n" >> $log
 }
 
-# Exception Catcher
-except () {
-    logger $1
-    return 1
-}
-
 # Assign timestamp to ensure var is a static point in time.
 timestp=$(timestamp)
-logger "Starting Build.\n"
+logger "Starting Build. Timestamp: $timestp\n"
 
 # Build the image using timestamp as tag.
-if /usr/bin/docker build /home/docker/nmap/nmap_docker_image -t docker.io/blairy/nmap:$timestp >> $log; then
-    logger "Build completed successfully.\n\n"
-else
-    logger "Build FAILED!! Aborting.\n\n"
-    exit 1
-fi
-
-# Test - If test pass, commit and push to github and Dockerhub.
-# if /home/docker/nmap/nmap_docker_image/test_script_nmap.py docker.io/blairy/nmap:$timestp >> $log; then
-#    logger "Tests completed successfully.\n\n"
-# else
-#    logger "******  WARNING!!  --  Tests FAILED!!  Aborting. ******\n\n"
-#    exit 1
-# fi
-
+function build() {
+cmd="/usr/bin/docker build /home/docker/nmap/nmap_docker_image -t docker.io/blairy/nmap:$timestp >> $log"
+logger "Running Docker Build.. cmd: $cmd"
+/usr/bin/docker build /home/docker/nmap/nmap_docker_image -t docker.io/blairy/nmap:$timestp >> $log || logger "Error! docker build failed"
+}
 
 # Push to github - Triggers builds in github and Dockerhub.
-# TODO: Make this a function and add better exception management.. 
-# only run this if the SSH function is successful.
 git () {
     git="/usr/bin/git -C /home/docker/nmap/nmap_docker_image/"
     $git -C '/home/docker/nmap/nmap_docker_image/' pull git@github.com:blairjames/nmap_docker_image.git >> $log || except "git pull failed!"
     $git add --all >> $log || except "git add failed!"
     $git commit -a -m 'Automatic build '$timestp >> $log || except "git commit failed!"
     $git push >> $log || except "git push failed!"
-}
-
-# Run the git transactions
-if git; then
-    logger "git completed successfully." 
-else
-    logger "git failed!!" 
-fi
+} 
 
 # Push the new tag to Dockerhub.
+function docker_push() {
 if docker push blairy/nmap:$timestp >> $log; then 
     logger "Docker push completed successfully.\n\n"
 else
     logger "Docker push FAILED!!\n\n"
     exit 1 
 fi
+}
 
-# Prune
+# Prune the git tree in the local dir
+function prune() {
+logger "Running git gc --prune"
 /usr/bin/git gc --prune || logger "Error!! - git gc failed!"
+}
 
-# All completed successfully
-logger "All completed successfully"
+
+function main() {
+build
+git
+docker_push
+prune
+logger "All complete."
+}
+
+main
