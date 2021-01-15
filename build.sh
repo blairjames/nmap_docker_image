1dac8e69af2410dd5597350f27de98df9055648b
Blair <blair@blairjames.com>
2021-01-16 09:03:05 +1000

    Automatic build 20210116_090301

--- build.sh
+++ build.sh
@@ -7,18 +7,18 @@ source /root/.ssh/agent/root || . /root/.ssh/agent/root
 declare -r log="/home/docker/nmap/nmap_docker_image/log_nmap_docker_image.log"
 
 # Generate timestamp
-timestamp () {
+function timestamp() {
   date +"%Y%m%d_%H%M%S"
 }
 
 # Log and Print
-logger () {
+logger() {
     printf "$1\n"
     printf "$(timestamp) - $1\n" >> $log
 }
 
 # Assign timestamp to ensure var is a static point in time.
-timestp=$(timestamp)
+declare -r timestp=$(timestamp)
 logger "Starting Build. Timestamp: $timestp\n"
 
 # Build the image using timestamp as tag.
@@ -30,28 +30,28 @@ function build() {
 }
 
 # Push to github - Triggers builds in github and Dockerhub.
-git () {
-    git="/usr/bin/git -C /home/docker/nmap/nmap_docker_image/"
-    $git -C '/home/docker/nmap/nmap_docker_image/' pull git@github.com:blairjames/nmap_docker_image.git >> $log || except "git pull failed!"
-    $git add --all >> $log || except "git add failed!"
-    $git commit -a -m 'Automatic build '$timestp >> $log || except "git commit failed!"
-    $git push >> $log || except "git push failed!"
+function git() {
+  git="/usr/bin/git -C /home/docker/nmap/nmap_docker_image/"
+  $git -C '/home/docker/nmap/nmap_docker_image/' pull git@github.com:blairjames/nmap_docker_image.git >> $log || except "git pull failed!"
+  $git add --all >> $log || except "git add failed!"
+  $git commit -a -m 'Automatic build '$timestp >> $log || except "git commit failed!"
+  $git push >> $log || except "git push failed!"
 } 
 
 # Push the new tag to Dockerhub.
 function docker_push() {
-if docker push blairy/nmap:$timestp >> $log; then 
+  if docker push blairy/nmap:$timestp >> $log; then 
     logger "Docker push completed successfully.\n\n"
-else
+  else
     logger "Docker push FAILED!!\n\n"
     exit 1 
-fi
+  fi
 }
 
 # Prune the git tree in the local dir
 function prune() {
-logger "Running git gc --prune"
-/usr/bin/git gc --prune || logger "Error!! - git gc failed!"
+  logger "Running git gc --prune"
+  /usr/bin/git gc --prune 
 }
 
 
